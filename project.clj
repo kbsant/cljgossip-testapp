fc62bfc0fdadb16e49e8f6f37a336541c189c5d5
kbsant <keansant@gmail.com>
2022-09-04 12:27:38 +0900

    bump deps

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A sample application using cljgossip and cljgossip-wsclient"
   :url "https://github.com/kbsant/cljgossip-testapp"
   :dependencies
-  [[org.clojure/clojure "1.10.0"]
+  [[org.clojure/clojure "1.11.1"]
    [cljgossip "0.1.0-SNAPSHOT"]
    [cljgossip/wsclient "0.1.0-SNAPSHOT"]]
   :repl-options {:init-ns cljgossip.testapp})
